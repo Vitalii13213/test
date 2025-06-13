289f77b25d58bea02a3e0bfabd7ca0731127565a
@ipz215_kvm <ipz215_kvm@student.ztu.edu.ua>
2025-06-13 15:05:27 +0300

    виправлення помилок

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm col-md-10 offset-md-2 px-md-4 py-3 bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">StyleHub</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
